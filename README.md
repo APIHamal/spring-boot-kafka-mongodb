fbe4ff505d2014281b64e6c7259e34aa052c12e7
idealist <66043887+APIHamal@users.noreply.github.com>
2021-07-21 08:56:31 +0800

    搭建项目骨架

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-kafka-mongodb
+springboot整合kafka与mongodb
---
+++ src/main/java/com/lizhengpeng/learn/springbootkafkamongodb/SpringBootKafkaMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.lizhengpeng.learn.springbootkafkamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaMongodbApplication.class, args);
+    }
+
+}
